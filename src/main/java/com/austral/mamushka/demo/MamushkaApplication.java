987d1386f9ec1bdd03ff297e49fecdaf57c01e76
MaritaMarita <89868671+MaritaMarita@users.noreply.github.com>
2022-05-02 18:40:59 -0300

    Add files via upload

---
+++ src/main/java/com/austral/mamushka/demo/MamushkaApplication.java
@@ -0,0 +1,13 @@
+package com.austral.mamushka.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamushkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MamushkaApplication.class, args);
+	}
+
+}
