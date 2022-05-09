90e7fe3067a3d3822a666e0e740c30fdc3c93456
MaritaMarita <mjcardinale@mail.austral.edu.ar>
2022-05-09 18:09:52 -0300

    Creamos package model y class Usuario

---
+++ src/main/java/com/austral/mamushka/demo/model/Usuario.java
@@ -0,0 +1,51 @@
+package com.austral.mamushka.demo.model;
+
+public class Usuario {
+
+    private Integer id;
+    private String nombre;
+    private String mail;
+    private String password;
+
+    public Usuario() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", mail='" + mail + '\'' +
+                '}';
+    }
+}
