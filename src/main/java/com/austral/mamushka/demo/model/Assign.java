f50526224d76a4fbc5a8786c5ff98f89d2ff1241
MaritaMarita <mjcardinale@mail.austral.edu.ar>
2022-05-09 18:49:31 -0300

    Creamos class Assign (la que va a relacionar las otras 2 clases: Usuario + Tarea)

---
+++ src/main/java/com/austral/mamushka/demo/model/Assign.java
@@ -0,0 +1,58 @@
+package com.austral.mamushka.demo.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "asignaciones")
+public class Assign {
+    @Id
+    private Integer id;
+    private Usuario usuario;
+    private Tarea tarea;
+    private boolean estado;
+
+    public Assign() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Tarea getTarea() {
+        return tarea;
+    }
+
+    public void setTarea(Tarea tarea) {
+        this.tarea = tarea;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Assign{" +
+                "id=" + id +
+                ", usuario=" + usuario +
+                ", tarea=" + tarea +
+                ", estado=" + estado +
+                '}';
+    }
+}
