e90941a2b6ac2796b3fd07f9880a962c2151d80c
MaritaMarita <mjcardinale@mail.austral.edu.ar>
2022-05-28 20:56:04 -0300

    creamos carpeta mamushka_backend

---
+++ mamushka_backend/src/test/java/com/austral/mamushka/demo/MamushkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.austral.mamushka.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MamushkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
